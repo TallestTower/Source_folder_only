ffb8b855e470b40e1a36ed362b5666128819a19d
XShadowmooreX <Arctic_strike@yahoo.com>
2019-11-25 15:53:01 -0700

    Weird Main stuffs
    'Nuff said.

--- src/MainTester.java
+++ src/MainTester.java
@@ -2,9 +2,15 @@ import window.CombatWindow;
 
 import javax.swing.*;
 
-public class MainTester {
-    public static void main(String[] args){
+public class Main {
 
+    public static void main(String[] args) {
+        System.out.println("Program Start!");
+        CombatWindow attack = new CombatWindow();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                attack.createAndShowGUI();
+            }
+        });
     }
-
 }
